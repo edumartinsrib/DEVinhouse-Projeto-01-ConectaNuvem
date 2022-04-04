28b3dabe4d713bc3903fe87109ab47ef922eab2b
Eduardo Martins <edumartinsrib@live.com>
2022-04-04 18:27:32 -0300

    changed icons and checkbox checked function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,49 +1,84 @@
+//Get Elementes
+const inputItem = document.getElementById("newItemInput");
+const formSubmit = document.getElementById("newItemForm");
+
+const arrItem = [];
+
+//Listener Events
+formSubmit.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const valueInput = inputItem.value.trim();
+
+  if (valueInput) {
+    const newID = addItem(valueInput, 0, 0);
+    newItem(valueInput, newID);
+    inputItem.value = "";
+    inputItem.focus();
+    createBtnEventListener();
+  }
+});
+
+function deleteById(array, id) {
+  try {
+    let element = document.getElementById(id);
+    let result = array.filter(function (el) {
+      return el.id == id;
+    });
+
+    for (let elemento of result) {
+      let index = array.indexOf(elemento);
+      array.splice(index, 1);
+    }
+
+    //delete element html
+    element.remove();
+  } catch (erro) {}
+}
+
+function createBtnEventListener() {
+  //get all ".btn" elements in array format
+  let btnDel = document.querySelectorAll(".delete");
+  for (let item of btnDel) {
+    item.addEventListener("click", () => {
+      deleteById(arrItem, item.name);
+    });
+  }
+}
 
 
-const newItemElement = (itemInput,idInput) => {
+
+const newItemElement = (itemInput, idInput) => {
   const divItem = document.createElement("div");
   divItem.classList.add("item");
+  divItem.setAttribute("id", idInput);
   divItem.innerHTML = `
-  <div class="content">
-    <input type="checkbox" name="${idInput}"/> <label for="${idInput}" class="lineThrough itemList"> ${itemInput}</label> 
+  <div class="content animate__animated animate__fadeIn" name="${idInput}">
+    <input type="checkbox" class="form-check-input" name="${idInput}"/> <label for="${idInput}" class="lineThrough itemList"> ${itemInput}</label> 
   </div>
   <div class="actions">
-    <button class="edit">-</button>
-    <button class="delete">x</button>
+
+    <button name="${idInput}" class="delete fa-solid fa-trash-can">X</button>
   </div>   
   `;
   return divItem;
 };
 
-
-let arrItem = [];
-
-function addItem(text, value, qtd){
-  const itemArray = {text, checked: false, id: Date.now(), value: 0, qtd: 0} 
+function addItem(text, value, qtd) {
+  const newID = Date.now() * Math.random();
+  const itemArray = { text, checked: false, id: newID, value: value, qtd: qtd };
   arrItem.push(itemArray);
-  console.log(arrItem);
+  return newID;
 }
 
-
-
-
-function newItem(itemInput) {
-  addItem(itemInput,0,0)
-  const addItemElement = newItemElement(itemInput,itemInput);
-  
+function newItem(itemInput, id) {
+  const addItemElement = newItemElement(itemInput, id);
   document.getElementById("items").appendChild(addItemElement);
+  updateStorage();
 }
-
 
 function updateStorage() {
   const itensJSON = JSON.stringify(arrItem);
   localStorage.setItem("listShop", itensJSON);
-
 }
 
 
-newItem("Cebola");
-newItem("Alho");
-newItem("Manjericão");
-
-updateStorage();
