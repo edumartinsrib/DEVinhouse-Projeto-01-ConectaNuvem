fe312f713f7f9f0a496f33a10cac9d3740e0ccee
Eduardo Martins <edumartinsrib@live.com>
2022-04-06 17:22:02 -0300

    Modal Done - Function Value Done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
-//Get Elementes
+/* Captura dos elementos do HTML */
+
 const modal = document.getElementById("myModal");
 const btnModal = document.getElementById("myBtn");
 const spanModal = document.getElementById("itemModal");
@@ -12,120 +13,125 @@ const listBox = document.getElementById("items");
 const deleteAll = document.getElementById("deleteAll");
 const deleteChecked = document.getElementById("deleteChecked");
 const nullInput = document.querySelector(".hidden");
-const listLocalStorage = "listShop";
-var idItem = '';
-let valorModal = 0;
+const valueTotal = document.getElementById('valueTotal');
 
+//Define Variáveis globais
+const listLocalStorage = "listShop";
+var idItem = "";
+let valorModal = 0;
 let arrItem = [];
 
-// search list in localStorange
+// Procura se há alguma lista salva no localStorage
 const listJSON = JSON.parse(localStorage.getItem(listLocalStorage));
 
 if (listJSON != null) {
-  arrItem = listJSON;
+  arrItem = listJSON; //se houver, o array do JS é atualizado com os valores do LocalStorage
 }
 
+//Evento click no botão 'Apagar itens comprados'
 deleteChecked.addEventListener("click", () => {
   removeItemChecked();
 });
 
+//Evento click no botão 'apagar todos os itens da lista'
 deleteAll.addEventListener("click", () => {
   deleteAllItems();
   renderScreen();
 });
 
-//function to render a screen with updates
+//Função principal e exclusiva para renderização dos elementos na tela
 function renderScreen() {
-  const items = JSON.parse(localStorage.getItem(listLocalStorage));
+  const items = JSON.parse(localStorage.getItem(listLocalStorage)); //Recupera os itens salvos no localStorage 
+  let htmlCode = ""; //variável que conterá o código HTML após iteração
+  let checkedValue = ""; //Variável para guardar o valor checked true/false na iteração
 
   if (!items) {
-    listBox.innerHTML = "";
+    listBox.innerHTML = ""; //Se não houver nada salvo, a lista é exibida vazia
     return;
   }
 
-  let htmlCode = "";
-  let checkedValue = "";
-
+  //Laço para iteração e criação dos elementos HTMLs, identificando cada elemento com seu ID e prefixo definido pela natureza do elemento
   items.forEach((item) => {
-    checkedValue = item.checked ? "checked" : "";
+    checkedValue = item.checked ? "checked" : ""; //chega se o campo deve estar marcado na renderização com base no valor do array
     htmlCode += `
     <li class="content input-group ${checkedValue}" id="${item.id}">
     <input type="checkbox" class="form-check-input chk" ${checkedValue} id="chk-${item.id}"/> 
     <span id="txt-${item.id} "class="lineThrough text itemList"> ${item.text}</span> 
-    <button id="btn-${item.id}" class="delete action fa-solid fa-trash-can">X</button> 
+    <button id="btn-${item.id}" class="delete action">X</button> 
     </li>`;
   });
-  listBox.innerHTML = htmlCode;
+  listBox.innerHTML = htmlCode; //renderiza os componentes na tela
 
   //-----------------Disparo dos eventos dinâmicos -----------------
 
-  //Deletar Item da Lista
+  //Cria as funções em todos os 'buttons' para deletar items da lista 
   var buttons = document.getElementsByClassName("delete"); // take all DOM elements buttons that have an 'remove'
 
   for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", removeItem);
   }
 
-  //edit item list
+  //Cria as funções em todos os elementos 'span' com a descrição do item, permitindo a edição após duploclick e fazendo a edição com o focusout
   var itemText = document.getElementsByClassName("lineThrough"); //  take all DOM elements span that have an 'Linethrough'
 
   for (var i = 0; i < itemText.length; i++) {
-    itemText[i].addEventListener("focusout", editItem);
-    itemText[i].addEventListener("dblclick", contentEditable);
+    itemText[i].addEventListener("focusout", editItem); //função focusout para alterar array/localstorage com base na descrição atualizada do elemento
+    itemText[i].addEventListener("dblclick", contentEditable); //função para liberar a edição do elemento após duploclick
   }
 
-  var buttons = document.getElementsByClassName("chk"); // take all DOM elements that have an 'chk' checkboxes
+  //Cria as funções para todos os checkboxs renderizados
+  var check = document.getElementsByClassName("chk"); // 
 
   for (var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", boxChecked);
+    check[i].addEventListener("click", boxChecked); //função para alterar o status checked do elemento no array/localstorage, assim como abrir modal para informar valores
   }
+  
+  //Por fim, é renderizado no footer o valor atualizado dos itens 'checkeds'
+
+  sumTotal()
+  
 }
 
-//variable's functions for Listeners - Edit/Delete
-
+//Variavel funcional com as seguintes funcionalidades: Alteração do array/localstorage com atualização do 'checked', 
+//inclusão de animações de movimento/alteração de cor do elemento, atualização do valor total da compra.
 var boxChecked = function () {
-  var parent = this.parentNode;
+  var parent = this.parentNode; //Obtém a referência do componente pai para inclusão das animações/alterações de background color
 
-  var id = this.getAttribute("id"); // variável id criada para receber o atual objeto-DOM referente ao id do checked
+  var id = this.getAttribute("id"); // variável id criada para receber o id do componente 'checked'
 
-  id = id.replace("chk-", "");
+  id = id.replace("chk-", ""); //Remove o prefixo determinado para obter o mesmo ID do array/localStorage 
 
-  arrItem = getList(); //
+  arrItem = getList(); //Atualiza o array com base no localStorage
 
   let result = arrItem.filter(function (el) {
-    return el.id == id;
+    return el.id == id; //filtra o array para obter um novo array apenas com o ID identificado
   });
 
   for (let element of result) {
     let index = arrItem.indexOf(element);
-    arrItem[index].checked = this.checked;
+    arrItem[index].checked = this.checked; //altera o checked do valor do array com base no elemento marcado 
   }
 
+  localStorage.setItem(listLocalStorage, JSON.stringify(arrItem)); //altera o checked do valor do localStorage com base no elemento marcado
+
+  //verifica itens marcados e adiciona/remove classes para animação e cor no elemento, a depender do estado.
   if (this.checked) {
     parent.classList.add("checked", "animate__animated", "animate__pulse");
-    idItem = id
+    idItem = id;
     modal.style.display = "block";
   } else {
     parent.classList.remove("checked", "animate__animated", "animate__pulse");
     parent.classList.add("noChecked");
-    idItem = '';
+   
+    //realiza a atualização do valor total em tela - é reexecutada a função, pois para os efeitos de animação ocorrerem, a tela não pode ser renderizada novamente
+    sumTotal()
+    //limpa o iD para o caso de um novo item ser marcado
+    idItem = "";
   }
-
-  localStorage.setItem(listLocalStorage, JSON.stringify(arrItem)); //
+  //
   //
 };
 
-function adicionarValor(ID) {
-  let result = arrItem.filter(function (el) {
-    return el.id == id;
-  });
-
-  for (let element of result) {
-    let index = arrItem.indexOf(element);
-    arrItem[index].checked = this.checked;
-    //textModal = arrItem[index].text
-  }
-}
 
 //Edit content text on doubleclick
 var contentEditable = function () {
@@ -185,7 +191,6 @@ function addItem(text, value, qtd) {
 const getList = function () {
   var getList_string = localStorage.getItem(listLocalStorage); // Pega o conteúdo/valor da chave 'todos' do 'localStorage' e armazena na variável 'todos_string'
   if (getList_string != null) {
-    // If  array not null, then do conversion JSON in array.
     return JSON.parse(getList_string);
   }
 };
@@ -195,17 +200,12 @@ renderScreen();
 
 //Listener Events
 
-// When the user clicks on the button, open the modal
-btnModal.addEventListener("click", () => {
-  modal.style.display = "block";
-});
-
 // When the user clicks on <span> (x), close the modal
 valueBtnModal.addEventListener("click", () => {
   if (valueInputModal.value) {
-    valorModal = parseInt(valueInputModal.value);
-    
+    valorModal = parseFloat(valueInputModal.value);
     addValueArr(idItem, valorModal);
+    valueInputModal.value = '';
     modal.style.display = "none";
   } else {
     return alert("Para prosseguir é necessário informar o valor do item!");
@@ -213,24 +213,18 @@ valueBtnModal.addEventListener("click", () => {
 });
 
 var addValueArr = (idItem, valor) => {
-  
   let result = arrItem.filter(function (el) {
     return el.id == idItem;
-    
   });
-
-  console.log("addValueArr ~ result", result)
 
   for (let element of result) {
     let index = arrItem.indexOf(element);
     arrItem[index].value = valor;
   }
+
   localStorage.setItem(listLocalStorage, JSON.stringify(arrItem)); //
   renderScreen(); // Render Screen
-  
-} 
-
-
+};
 
 formSubmit.addEventListener("submit", (e) => {
   //capture submit
@@ -257,8 +251,8 @@ function deleteAllItems() {
 }
 
 function deleteCheckeds() {
-  var id = this.getAttribute("id"); 
-  
+  var id = this.getAttribute("id");
+
   id = id.replace("btn-", "");
   arrItem = getList(); //
 
@@ -268,9 +262,24 @@ function deleteCheckeds() {
 
   for (let element of result) {
     let index = arrItem.indexOf(element);
-    arrItem[index].text = text;
+    arrItem.splice(index, 1);
   }
-
+  
   localStorage.setItem(listLocalStorage, JSON.stringify(arrItem)); //
   renderScreen(); // Render Screen
-};
+}
+
+function sumTotal(){ 
+  
+  arrItem = getList();
+
+  let result = arrItem.filter(function (el) {
+    return el.checked == true;
+  });
+  
+  const accTotal = result.reduce((acc, value) => acc + value.value, 0)
+
+  accTotal ? valueTotal.textContent = accTotal.toLocaleString('pt-br', {style: 'currency',currency: 'BRL', minimumFractionDigits: 2}) : valueTotal.textContent = '0,00'
+  
+
+}
