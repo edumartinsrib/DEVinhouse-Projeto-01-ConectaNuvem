ada2bca78f590ab14422dd715b74f25e4de6a7d7
Eduardo Martins Ribeiro <edumartinsrib@live.com>
2022-04-09 19:34:19 -0300

    song finish add

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -1,14 +1,17 @@
 export function songEffect(song) {
-    const songEffectCash = new Audio("/assets/audio/Money.wav");
-    const songEffectTrash = new Audio("/assets/audio/Trash.wav");
+  const songEffectCash = new Audio("/assets/audio/Money.wav");
+  const songEffectTrash = new Audio("/assets/audio/Trash.wav");
+  const songEffectFinish = new Audio("/assets/audio/Finish.wav");
   
-    switch (song) {
-      case "trash":
-        songEffectTrash.play();
-        break;
-      case "cash":
-        songEffectCash.play();
-        break;
-    }
+  switch (song) {
+    case "trash":
+      songEffectTrash.play();
+      break;
+    case "cash":
+      songEffectCash.play();
+      break;
+    case "finish":
+      songEffectFinish.play();
+      break;
   }
-  
+}
